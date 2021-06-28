5578fb9ac901145cb11de3ec67670ca761424c64
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-06-28 17:24:19 +0300

    removing warnings from the react app

--- src/components/todo/Header.js
+++ src/components/todo/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 const Header = props => {
   return (
     <React.Fragment>

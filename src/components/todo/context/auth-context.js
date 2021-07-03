2caa07281ab632f6a6feb934580c97b99679f89d
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-07-03 15:26:43 +0300

    added the login handler in the context auth

--- src/components/todo/context/auth-context.js
+++ src/components/todo/context/auth-context.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cookie from 'react-cookie';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
 
 export const AuthContext = React.useContext();
+const API = 'https://api-js401.herokuapp.com';
 
 const AuthProvider = props => {
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+
+  const login = async (username, password) => {
+    try {
+      let response = await axios({
+        method: 'POST',
+        url: `${API}/sigin`,
+        headers: {
+          mode: 'cors',
+          cache: 'no-cache',
+        },
+        auth: {
+          username: username,
+          password: password,
+        },
+      });
+      validateToekn(response.data.token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
 };
 
